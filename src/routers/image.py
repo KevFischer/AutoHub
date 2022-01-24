50657512bd6275357a602f8d8e42b7725e413a3d
KevFischer <kevinf260800mail.com>
2022-01-24 18:10:11 +0100

    File fix maybe?

--- src/routers/image.py
+++ src/routers/image.py
@@ -14,8 +14,8 @@ async def upload_post(id: int, file: UploadFile = File(...)):
     content = await file.read()
     filename = str(uuid4()) + os.path.splitext(file.filename)[1]
     if file.content_type.split("/")[0] == "image":
-        print(f"{directory}/" + filename)
-        with open(f"{directory}/" + filename, "wb") as f:
+        print(f"{directory}" + filename)
+        with open(f"{directory}" + filename, "wb") as f:
             f.write(content)
         return {"filename": filename}
     raise HTTPException(status_code=422)
