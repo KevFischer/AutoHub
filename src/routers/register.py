5faa3897d4ee6352d6aeb2853e9943ba4c4ae260
KevFischer <kevinf260800mail.com>
2022-02-01 21:24:47 +0100

    Removed secure login

--- src/routers/register.py
+++ src/routers/register.py
@@ -44,37 +44,3 @@ def register(request: RequestRegister, db: Session = Depends(init_db)):
     return RespondLogin(
         token=generate_token(request.email, db)
     )
-
-
-@router.post("/hash/")
-def register(request: RequestRegister, db: Session = Depends(init_db)):
-    """
-    Register function to register a new Account entity to the database. \n
-    :param request: Request Body received by Frontend application \n
-    :param db: Database to interact with
-    """
-    # Check if mandatory columns are available.
-    if request.username and request.password and request.email is None:
-        raise HTTPException(status_code=422)
-    # Check if there is already an account registered to the e-mail address.
-    if db.query(Account).filter(Account.email == request.email).first() is not None:
-        raise HTTPException(status_code=409)
-    # Check if password is OK
-    if not check_password(request.password):
-        raise HTTPException(status_code=422)
-    # Check if email is an email.
-    if "@" not in request.email:
-        raise HTTPException(status_code=422)
-    # Create new Account-object with values from the request.
-    new_account = Account(
-        username=request.username,
-        email=request.email,
-        phone=request.phone,
-        password=request.password
-    )
-    # Finally, add the new account to the database and commit the changes.
-    db.add(new_account)
-    db.commit()
-    return RespondLogin(
-        token=generate_token(request.email, db)
-    )
