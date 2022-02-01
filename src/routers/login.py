4d22af0dc390b42a247c55f397c0db26befd350a
KevFischer <kevinf260800mail.com>
2022-02-01 21:12:22 +0100

    Added secure login route

--- src/routers/login.py
+++ src/routers/login.py
@@ -31,3 +31,24 @@ def login(request: RequestLogin, db: Session = Depends(init_db)):
     return RespondLogin(
         token=generate_token(request.email, db)
     )
+
+
+@router.post("/hash/", response_model=RespondLogin)
+def login(request: RequestLogin, db: Session = Depends(init_db)):
+    """
+    Function to log in into an account. \n
+    :param request: Login request body \n
+    :param db: Database to interact with \n
+    :return: JWT token for user privileges
+    """
+    if request.email == "" or request.password == "":
+        raise HTTPException(status_code=422)
+    if request.email is None or request.password is None:
+        raise HTTPException(status_code=422)
+    if db.query(Account).filter(Account.email == request.email).first() is None:
+        raise HTTPException(status_code=404)
+    if db.query(Account.password).filter(Account.email == request.email).first()[0] != request.password:
+        raise HTTPException(status_code=404)
+    return RespondLogin(
+        token=generate_token(request.email, db)
+    )
